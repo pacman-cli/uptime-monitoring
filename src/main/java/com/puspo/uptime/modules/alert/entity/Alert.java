577ec946db2660ecf223ff9e541e8c390ab2590b
pacman-cli <puspopuspo520@gmail.com>
2026-02-24 22:11:59 +0600

    Added default cors

--- src/main/java/com/puspo/uptime/modules/alert/entity/Alert.java
+++ src/main/java/com/puspo/uptime/modules/alert/entity/Alert.java
@@ -1,8 +1,22 @@
 package com.puspo.uptime.modules.alert.entity;
 
+import com.puspo.uptime.common.BaseEntity;
 import com.puspo.uptime.modules.monitor.entity.Monitor;
-import jakarta.persistence.*;
-import lombok.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "alerts")
@@ -11,7 +25,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Alert {
+public class Alert extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
