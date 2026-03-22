da428fe2d0afe00f15d7adaff416988901c7b91b
pacman-cli <puspopuspo520@gmail.com>
2026-03-22 20:53:36 +0600

    import enable async

--- server/src/main/java/com/puspo/uptime/UptimeApplication.java
+++ server/src/main/java/com/puspo/uptime/UptimeApplication.java
@@ -3,6 +3,7 @@ package com.puspo.uptime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
