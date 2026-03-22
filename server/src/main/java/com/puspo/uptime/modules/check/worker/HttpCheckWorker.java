c648b43f8aa746fd7bdeb39cfe5dbdb11a845b35
pacman-cli <puspopuspo520@gmail.com>
2026-03-22 22:47:52 +0600

    this.emailNotificationService

--- server/src/main/java/com/puspo/uptime/modules/check/worker/HttpCheckWorker.java
+++ server/src/main/java/com/puspo/uptime/modules/check/worker/HttpCheckWorker.java
@@ -26,6 +26,7 @@ public class HttpCheckWorker {
         this.monitorLogRepository = monitorLogRepository;
         this.webClient = webClient;
         this.alertService = alertService;
+        this.emailNotificationService = emailNotificationService;
     }
 
     // Check the monitor
