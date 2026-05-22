a1bddde563512875d20a1408b7c7f994f310d32b
pacman-cli <puspopuspo520@gmail.com>
2026-05-23 03:22:12 +0600

    chore: update .gitignore, commit RegisterRequest changes

--- server/src/main/java/com/puspo/uptime/modules/auth/dto/RegisterRequest.java
+++ server/src/main/java/com/puspo/uptime/modules/auth/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.puspo.uptime.modules.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,5 +19,6 @@ public class RegisterRequest {
   private String email;
 
   @NotBlank(message = "Password is required")
+  @Size(min = 8, message = "Password must be at least 8 characters")
   private String password;
 }
